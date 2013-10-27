785bcf87ede715217ce399e17227baa9d2c43950
asy524 <fjshsjf@gmail.com>
2013-10-27 16:15:45 +0900

    modified js/papero.js to show errors

--- js/papero.js
+++ js/papero.js
@@ -7,16 +7,18 @@ $(function(){
             var text = $(this).val();
             //console.log(text);                                                                                                                              
             $(this).val("");
-            $.get("http://shinzan.human.waseda.ac.jp:8091/chat/" + text,
+	    $.get("http://shinzan.human.waseda.ac.jp:8091/chat/" + text,
                   function(reply){
-                      $("#chatlog").text("...");
-
-                      setTimeout(function(){
-                          $("#chatlog").text(reply);
-                      },100);
-                      return false;
-                  });
-	    }
+		      $("#chatlog").text("...");
+		      
+		      setTimeout(function(){
+                          $("#chatlog").text(reply).css("color","Black");
+		      },100);
+		      return false;
+                  }).fail(function(){
+		      $("#chatlog").text("[ ネットワークに接続できていないかパペロが休んでます ]").css("color","RED");
+		  });
+	}
     }).focus(function(){
         $(this).val("");
     }).blur(function(){
